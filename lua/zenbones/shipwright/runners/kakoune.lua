22aad6655de236a18f4ddcee7b70100d0e8fa468
Parker Rueve <parkerrueve@gmail.com>
2024-02-26 23:27:36 -0500

    working (if incomplete) kakoune themes

--- lua/zenbones/shipwright/runners/kakoune.lua
+++ lua/zenbones/shipwright/runners/kakoune.lua
@@ -2,13 +2,7 @@ local helpers = require "shipwright.transform.helpers"
 local start_template = [[
 # $n theme
 
-evaluate-commands %sh{
 ]]
-
-local end_template = [[
-}
-]]
-
 
 local face_mappings = {
   ["value"]              = specs.Constant,
@@ -17,7 +11,7 @@ local face_mappings = {
   ["module"]             = specs.Type,
   ["function"]           = specs.Function,
   ["string"]             = specs.Constant,
-  ["keyword"]            = specs.Keyword,
+  ["keyword"]            = specs.Statement,
   ["operator"]           = specs.Function,
   ["attribute"]          = specs.PreProc,
   ["comment"]            = specs.Comment,
@@ -27,7 +21,7 @@ local face_mappings = {
 
   -- Markdown highlighting
   ["title"]              = specs.Special,
-  ["header"]             = specs.Keyword,
+  ["header"]             = specs.Statement,
   ["mono"]               = specs.Constant,
   ["block"]              = specs.Normal,
   ["link"]               = specs.Normal,
@@ -62,82 +56,75 @@ local face_mappings = {
   ["Whitespace"]         = specs.Normal,
 }
 
-local template = [[
-   gray="rgb:928374"
-    red="rgb:fb4934"
-    green="rgb:b8bb26"
-    yellow="rgb:fabd2f"
-    blue="rgb:83a598"
-    purple="rgb:d3869b"
-    aqua="rgb:8ec07c"
-    orange="rgb:fe8019"
+local function create_face(name)
+  local face = { name = name, body = "" }
 
-    bg="rgb:282828"
-    bg_alpha="rgba:282828a0"
-    bg1="rgb:3c3836"
-    bg2="rgb:504945"
-    bg3="rgb:665c54"
-    bg4="rgb:7c6f64"
+  function face.append(self, value)
+		self.body = self.body .. value
+  end
 
-    fg="rgb:ebdbb2"
-    fg_alpha="rgba:ebdbb2a0"
-    fg0="rgb:fbf1c7"
-    fg2="rgb:d5c4a1"
-    fg3="rgb:bdae93"
-    fg4="rgb:a89984"
+  function face.append_color(self, hsl)
+    str = "" .. hsl
+    str = str:gsub("#", "rgb:")
+		self:append(str)
+  end
 
-    echo "
-        # Code highlighting
-        face global value         ${purple}
-        face global type          ${yellow}
-        face global variable      ${blue}
-        face global module        ${green}
-        face global function      ${fg}
-        face global string        ${green}
-        face global keyword       ${red}
-        face global operator      ${fg}
-        face global attribute     ${orange}
-        face global comment       ${gray}+i
-        face global documentation comment
-        face global meta          ${aqua}
-        face global builtin       ${fg}+b
+  function face.print(self)
+    return "face global " .. self.name .. " " .. self.body
+  end
 
-        # Markdown highlighting
-        face global title     ${green}+b
-        face global header    ${orange}
-        face global mono      ${fg4}
-        face global block     ${aqua}
-        face global link      ${blue}+u
-        face global bullet    ${yellow}
-        face global list      ${fg}
+ 	return face
+end
 
-        face global Default            ${fg},${bg}
-        face global PrimarySelection   ${fg_alpha},${blue}+g
-        face global SecondarySelection ${bg_alpha},${blue}+g
-        face global PrimaryCursor      ${bg},${fg}+fg
-        face global SecondaryCursor    ${bg},${bg4}+fg
-        face global PrimaryCursorEol   ${bg},${fg4}+fg
-        face global SecondaryCursorEol ${bg},${bg2}+fg
-        face global LineNumbers        ${bg4}
-        face global LineNumberCursor   ${yellow},${bg1}
-        face global LineNumbersWrapped ${bg1}
-        face global MenuForeground     ${bg2},${blue}
-        face global MenuBackground     ${fg},${bg2}
-        face global MenuInfo           ${bg}
-        face global Information        ${bg},${fg}
-        face global Error              ${bg},${red}
-        face global DiagnosticError    ${red}
-        face global DiagnosticWarning  ${yellow}
-        face global StatusLine         ${fg},${bg}
-        face global StatusLineMode     ${yellow}+b
-        face global StatusLineInfo     ${purple}
-        face global StatusLineValue    ${red}
-        face global StatusCursor       ${bg},${fg}
-        face global Prompt             ${yellow}
-        face global MatchingChar       ${fg},${bg3}+b
-        face global BufferPadding      ${bg2},${bg}
-        face global Whitespace         ${bg2}+f
-    "
-}
+local function create_kakoune_theme(name)
+  return function(colorscheme)
+    local result = ""
 
-]]
+    for name, spec in pairs(face_mappings) do
+      local face = create_face(name)
+
+      if spec.fg then
+        face:append_color(spec.fg)
+      end
+
+      if spec.bg then
+        if not spec.fg then
+          face:append("default")
+        end
+
+        face:append(",")
+        face:append_color(spec.bg)
+      end
+
+      local attributes = ""
+
+      if spec.gui and string.find(spec.gui, "bold") then
+        attributes = attributes .. "b"
+      end
+
+      if spec.gui and string.find(spec.gui, "italic") then
+				if not string.find(spec.gui, "bold") then
+  				attributes = attributes .. "i"
+				end
+      end
+
+      if string.len(attributes) > 0 then
+        face:append("+")
+        face:append(attributes)
+      end
+
+      result = result .. face:print() .. "\n"
+    end
+
+    template = helpers.apply_template(start_template, { n = name }) .. result
+
+    return { template }
+  end
+end
+
+---@diagnostic disable: undefined-global
+-- selene: allow(undefined_variable)
+run({ specs, term }, create_kakoune_theme(name), { overwrite, string.format("extras/kakoune/%s.kak", name) })
+-- selene: deny(undefined_variable)
+---@diagnostic enable: undefined-global
+--
